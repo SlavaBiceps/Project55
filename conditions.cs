7276dbdc73ea62fceeed67a92b7911dd9fa97a88
SlavaBiceps <slava_ivanov_cs_12321@mail.ru>
2021-12-17 10:19:23 +0300

    first

---
+++ conditions.cs
@@ -0,0 +1,221 @@
+﻿namespace Project55
+{
+    public class conditions
+    {
+        private static string condition_number;
+        public static bool check_condition(string get_condition_number)
+        {
+            bool answer = false;
+            condition_number = get_condition_number;
+            answer = Rilan();
+            if (condition_number == "Открыть сумку")
+            {
+                return true;
+            }
+            if (condition_number == "Закрыть сумку")
+            {
+                return true;
+            }
+            return answer;
+        }
+
+        public static bool Rilan()
+        {
+            if (condition_number == "Зайти в темный переулок")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Рыбная")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Рыбная2")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Рыбная3")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Рыбная4")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Трех Кинжалов")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Трех Кинжалов2")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Трех Кинжалов3")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Трех Кинжалов4")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Трех Кинжалов5")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Морозная")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Морозная2")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Морозная3")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Морозная4")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Морозная5")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Морозная6")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Зларис")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Зларис2")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Зларис3")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Зларис4")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Зларис5")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Цветочная")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Цветочная2")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Цветочная3")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Цветочная4")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Цветочная5")
+            {
+                return true;
+            }
+            if (condition_number == "Торговая площадь Рилана")
+            {
+                return true;
+            }
+            if (condition_number == "Площадь Рилана")
+            {
+                return true;
+            }
+            if (condition_number == "Торговая площадь Рилана2")
+            {
+                return true;
+            }
+            if (condition_number == "Ветренная Аллея")
+            {
+                return true;
+            }
+            if (condition_number == "Ветренная Аллея2")
+            {
+                return true;
+            }
+            if (condition_number == "Ветренная Аллея3")
+            {
+                return true;
+            }
+            if (condition_number == "Ветренная Аллея4")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Новой Надежды")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Новой Надежды2")
+            {
+                return true;
+            }
+            if (condition_number == "Улица Новой Надежды3")
+            {
+                return true;
+            }
+            if (condition_number == "Мост к Академии")
+            {
+                return true;
+            }
+            if (condition_number == "Мост к Академии2")
+            {
+                return true;
+            }
+            if (condition_number == "Мост к Академии3")
+            {
+                return true;
+            }
+            if (condition_number == "Мост к Академии4")
+            {
+                return true;
+            }
+            if (condition_number == "Рынок")
+            {
+                return true;
+            }
+            if (condition_number == "Переулок Колсо")
+            {
+                return true;
+            }
+            if (condition_number == "Переулок Колсо2")
+            {
+                return true;
+            }
+            if (condition_number == "Переулок Колсо3")
+            {
+                return true;
+            }
+            if (condition_number == "Порт Рилана")
+            {
+                return true;
+            }
+            if (condition_number == "Таверна Пьяный Дракон")
+            {
+                return true;
+            }
+            //
+            if (condition_number == "Rilan_bridge_closed.Уйти")
+            {
+                return true;
+            }
+            if (condition_number == "Rilan_bridge_closed.Баг")
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
